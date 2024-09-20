2dc3ac18aa832555d95abbbcd729d0b3bf16acaf
Samir Chaulagain <royal.chaulagain321@gmail.com>
2024-09-21 00:09:56 +0545

    Creating Api endpoints:working on backend(django)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # React-django
+Day 5
+This POST request's from Django rest framework. We setup some API endpoints that will be used to send information to and create new entires in our database using serializers and session to store temporary id of user .IN views .py.
--- room_backend/myapp/views.py
+++ room_backend/myapp/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics,status
 from .models import Room
 from .serializers import RoomSerializer,CreateRoomSerializer
 from .models import Room
@@ -22,15 +22,18 @@ class CreateRoomView(APIView):
         if serializer.is_valid():
             skip_votes=serializer.data.get("skip_votes")
             guest_pause=serializer.data.get("guest_pause")
-            host = self.session.session_key
+            host = self.request.session.session_key
             queryset = Room.objects.filter(host=host)
             if queryset.exists():
                 room= queryset[0]
                 room.guest_pause=guest_pause
                 room.skip_votes=skip_votes
                 room.save(update_fields=["guest_pause","skip_votes"])
+                return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
             else:
-                room=Room(host=host,guest_pause=guest_pause,skip_votes=skip_votes)
+                room = Room(host=host, guest_pause=guest_pause,
+                            skip_votes=skip_votes)
                 room.save()
+                return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
 
-            return Response(RoomSerializer(room).data,status=status.HTTP_204_OK)
+        return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
