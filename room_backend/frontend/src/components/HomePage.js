fd03e01ddb7046a6a66cb65f94607c989eaddcaf
Samir Chaulagain <royal.chaulagain321@gmail.com>
2024-10-02 05:09:49 +0545

    Calling API Endpoints From React

--- room_backend/frontend/src/components/HomePage.js
+++ room_backend/frontend/src/components/HomePage.js
@@ -4,6 +4,7 @@ import RoomPage from "./RoomPage";
 import {
     BrowserRouter as Router, Routes, Route
 } from "react-router-dom";
+import Room from './Room';
 
 export default class HomePage extends Component {
     constructor(props) {
@@ -17,6 +18,8 @@ export default class HomePage extends Component {
                     <Route path="/" element={<h1>This is home page</h1>} />
                     <Route path="/join" element={<RoomPage />} />
                     <Route path="/create" element={<CreateRoom />} />
+                    <Route path="/room/:roomCode" element={<Room />} />
+
                 </Routes>
             </Router>
         );
